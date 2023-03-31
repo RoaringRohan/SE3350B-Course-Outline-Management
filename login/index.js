fb5cdd9ff7a123c3d05b93564fcf8444d8d14f18
Rohan Datta <90877887+RoaringRohan@users.noreply.github.com>
2023-03-31 19:34:43 -0400

    init

---
+++ login/index.js
@@ -0,0 +1,123 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBNYfWsGWI6-q8by0KkHK05Joh4vIC25ek",
+    authDomain: "courseoutline-a3649.firebaseapp.com",
+    projectId: "courseoutline-a3649",
+    storageBucket: "courseoutline-a3649.appspot.com",
+    messagingSenderId: "711912027557",
+    appId: "1:711912027557:web:656b73c6539332c7cb644c",
+    measurementId: "G-7Q0C7PM4NG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
+  var db = firebase.firestore();
+  var collectionRef = db.collection("users");
+  
+  // Set up our register function
+  function register () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    full_name = document.getElementById('full_name').value
+    var type = document.getElementById('type').value;
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is incorrect')
+      return
+    }
+    if (validate_field(full_name) == false || validate_field(type) == false) {
+      alert('One or More Extra Fields is left empty.')
+      return
+    }
+   
+    collectionRef.doc(email).set({
+      full_name: full_name,
+      email: email,
+      password: password,
+      type_of_user: type,
+      last_login: Date.now()
+    }).then(function() {
+      // Done
+      alert('User has been created successfully')
+    }).catch(function(error) {
+      var error_message = error.message
+      alert(error_message)
+    })
+  }
+  
+  function login() {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or password incorrect')
+      return
+    }
+    
+    // Retrieve the user's password from the database and compare it with the input password
+    collectionRef.doc(email).get().then(function(doc) {
+      if (doc.exists) {
+        var user = doc.data();
+        if (user.password == password) {
+          // Password is correct, update last login and redirect to new page
+          collectionRef.doc(email).update({
+            last_login: Date.now()
+          }).then(function() {
+            // Navigate to new HTML page
+            //<!-- USE YOUR LOCAL FILE LOCATION FOR THIS DURING DEMO VIDEO-->
+            window.location.href = "C:/Users/Rohan Datta/code/se3350_groupproject-se3350-team-19/src/dashboard/dashboard.html";
+          }).catch(function(error) {
+            var error_message = error.message
+            alert(error_message)
+          })
+        } else {
+          alert('Email or password incorrect')
+        }
+      } else {
+        alert('User not found')
+      }
+    }).catch(function(error) {
+      var error_message = error.message
+      alert(error_message)
+    })
+  }
+  
+  
+  
+  
+  // Validate Functions
+  function validate_email(email) {
+    expression = /^[^@]+@\w+(\.\w+)+\w$/
+    if (expression.test(email) == true) {
+      // Email is good
+      return true
+    } else {
+      // Email is not good
+      return false
+    }
+  }
+  
+  function validate_password(password) {
+    // Firebase only accepts lengths greater than 6
+    if (password < 6) {
+      return false
+    } else {
+      return true
+    }
+  }
+  
+  function validate_field(field) {
+    if (field == null) {
+      return false
+    }
+  
+    if (field.length <= 0) {
+      return false
+    } else {
+      return true
+    }
+  }
---
+++ main.js
@@ -0,0 +1,155 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNYfWsGWI6-q8by0KkHK05Joh4vIC25ek",
+  authDomain: "courseoutline-a3649.firebaseapp.com",
+  projectId: "courseoutline-a3649",
+  storageBucket: "courseoutline-a3649.appspot.com",
+  messagingSenderId: "711912027557",
+  appId: "1:711912027557:web:656b73c6539332c7cb644c",
+  measurementId: "G-7Q0C7PM4NG"
+};
+
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+
+//Variable to access database collection
+const db2 = firestore.collection("documents");
+
+
+//Get Submit Form
+let submitButton = document.getElementById("submit");
+
+//Create Event Listener To Allow Form Submission
+submitButton.addEventListener("click", (e) => {
+  //Prevent Default Form Submission Behavior
+  e.preventDefault();
+
+
+  let hislog = document.getElementById("historylog");
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' '+time + ' <br />';
+
+  hislog.innerHTML += dateTime;
+
+  let hl = document.getElementById("historylog").innerHTML;
+
+
+
+
+
+  let i0 = document.getElementById("indicator0").innerText;
+  let i1 = document.getElementById("indicator1").innerText;
+  let i2 = document.getElementById("indicator2").innerText;
+  let i3 = document.getElementById("indicator3").innerText;
+  let i4 = document.getElementById("indicator4").innerText;
+  let i5 = document.getElementById("indicator5").innerText;
+  let i6 = document.getElementById("indicator6").innerText;
+  let i7 = document.getElementById("indicator7").innerText;
+  let i8 = document.getElementById("indicator8").innerText;
+  let i9 = document.getElementById("indicator9").innerText;
+  let i10 = document.getElementById("indicator10").innerText;
+  let i11 = document.getElementById("indicator11").innerText;
+
+  let complete1 = document.getElementById("completed1").value;
+  let complete2 = document.getElementById("completed2").value;
+  let complete3 = document.getElementById("completed3").value;
+  let complete4 = document.getElementById("completed4").value;
+
+
+  let outline = document.getElementById("outline").value;
+  let edit = document.getElementById("editormain").innerHTML;
+  let feedback = document.getElementById("feedback").value;
+
+  firestore
+
+  //Save Form Data To Firebase
+  db2.doc(outline)
+    .set({
+
+        indicator0: i0,
+        indicator1: i1,
+        indicator2: i2,
+        indicator3: i3,
+        indicator4: i4,
+        indicator6: i5,
+        indicator7: i6,
+        indicator5: i7,
+        indicator8: i8,
+        indicator9: i9,
+        indicator10: i10,
+        indicator11: i11,
+
+        completed1: complete1,
+        completed2: complete2,
+        completed3: complete3,
+        completed4: complete4,
+        doc : edit,
+        justifications: feedback,
+        hl: hl
+
+
+
+    })
+    .then(() => { })
+    .catch((error) => {
+      console.log(error);
+    });
+  
+  //alert
+  alert("Your Form Has Been Submitted Successfully");
+
+  //clear form after submission
+  function clearForm() {
+    document.getElementById("clearFrom").reset();
+  }
+  clearForm()
+});
+
+let newoutline = document.getElementById("new");
+
+ newoutline.addEventListener("click",(e)=>{
+   window.location.reload();
+ });
+
+function setTime(num){
+    console.log("hello")
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime =  '/n' + date+' '+time + '/n';
+     
+    console.log(dateTime)
+
+    var checkboxvar = document.getElementById("completed" + num);
+    
+    if (checkboxvar.checked == true){
+       
+        document.getElementById("completed" + num + "0").innerHTML = "Completed on: " + dateTime;
+    }
+    else{
+        document.getElementById("completed" + num + "0").innerHTML = "Completed on: ";
+    }
+}
+
+function getDocuments(id) {
+	const q = query(collection(firestore, 'docs', id, 'documents'));
+	const unsubscribe = onSnapshot(q, (querySnapshot) => {
+		querySnapshot.forEach((doc) => {
+			let dcus = doc.data();
+			dcus.id = doc.id;
+			holdDoc.push(dcus);
+			showDoc();
+		});
+	});
+}
+
+
